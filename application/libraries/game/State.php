9c5de3a3248a84108bdbaddadeb3e37c33385b12
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2017-02-21 16:41:14 +0200

    interim commit

--- application/libraries/game/State.php
+++ application/libraries/game/State.php
@@ -10,13 +10,9 @@ namespace TicTacToe\application\libraries\game;
 
 
 use TicTacToe\application\libraries\game\enums\StateEnum;
-use TicTacToe\application\model\Entities\Player;
 
 class State
 {
-    /** @var Player **/
-    protected $CurrentPlayer;
-
     /** @var string */
     protected $Turn;
 
@@ -28,6 +24,12 @@ class State
 
     /** @var array **/
     protected $Board = [];
+
+    /** @var int */
+    protected $Player1Score = 1;
+
+    /** @var int */
+    protected $Player2Score = 2;
 
     public function State() { }
 
@@ -43,12 +45,55 @@ class State
         return $this->Round;
     }
 
-    public function IsTerminal()
+    /**
+     * @return bool
+     */
+    private function IsTerminal() : bool
     {
-        
+        $board = $this->Board;
+
+        for ($r = 0; $r <= 6; $r+=3)
+        {
+            $p = $board[$r];
+            if ($p != null && $p == $board[$r + 1] &&  $p == $board[$r + 2])
+            {
+                $this->DeclareWinner($p);
+                return true;
+            }
+        }
+        unset($r, $p);
+
+        for ($c = 0; $c <= 2; $c++)
+        {
+            $p = $board[$c];
+            if ($p != null && $p == $board[$c + 3] && $p == $board[$c + 6])
+            {
+                $this->DeclareWinner($p);
+                return true;
+            }
+        }
+        unset($c, $p);
+
+        for ($r = 0, $d = 4; $r <= 2 ; $r+=2, $d-=2)
+        {
+            $p = $board[$r];
+            if ($p != null && $p == $board[$r + $d] && $p == $board[$r + 2 * $d])
+            {
+                $this->DeclareWinner($p);
+                return true;
+            }
+        }
+        unset($r, $d, $p);
+
+        return false;
     }
 
-    public function AdvanceRound()
+    private function DeclareWinner($piece)
+    {
+        $this->State = ($piece == 'X') ? StateEnum::Player1Wins : StateEnum::Player2Wins;
+    }
+
+    private function AdvanceRound()
     {
         $this->Turn = "X" ? "O" : "X";
         $this->Round++;
